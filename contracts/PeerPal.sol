f7028e517afedb3e7ecb11e858fdcc6389a2badc
fchambi <fchambi70@gmail.com>
2023-11-19 01:36:41 +0300

    Escrow 5.0 PeerPalflattened Verified UPDATE in Wei

--- contracts/PeerPal.sol
+++ contracts/PeerPal.sol
@@ -1,9 +1,8 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-contract Escrow {
-    uint256 public counter;
-    
+contract PeerPal {
+    uint256 public counter;   
     address public owner;
     IERC20 public _token;    
     uint256 public feeSeller;
@@ -48,7 +47,7 @@ enum EscrowStatus {
         Arbitration
     }
 
-    struct Escrow {
+struct Escrow {
         address payable buyer; 
         address payable seller; 
         uint256 value; 
@@ -97,12 +96,11 @@ enum EscrowStatus {
     ) external payable virtual {
         uint256 _orderId = counter + 1;
         require(msg.sender != _seller, "seller cannot be the same as buyer");
-
         uint8 _decimals = 18;
         uint256 _amountFeeBuyer = ((_value * (feeBuyer * 10 ** _decimals)) /
             (100 * 10 ** _decimals)) / 1000;
         feeBuyer = _amountFeeBuyer;
-        require((_value * 10 ** _decimals) <= msg.value, "Incorrect amount");
+        require((_value ) <= msg.value, "Incorrect amount");
 
         string memory _idImage = "NO IMAGE" ;
 
@@ -121,7 +119,6 @@ enum EscrowStatus {
         emit EscrowDeposit(_orderId, escrows[_orderId]);
 
     }
-
     function releaseEscrowNativeCoin(
         uint _orderId
     ) external onlyBuyer(_orderId) {
@@ -148,10 +145,8 @@ enum EscrowStatus {
 
 
         escrows[_orderId].status = EscrowStatus.Completed;
-
-
         (bool sent, ) = escrows[_orderId].seller.call{
-            value: (escrows[_orderId].value * 10 ** _decimals) - _amountFeeSeller
+            value: (escrows[_orderId].value ) - _amountFeeSeller
         }("");
         
         require(sent, "Transfer failed.");
@@ -160,14 +155,11 @@ enum EscrowStatus {
 
 
     }
-
-
    function refundBuyerNativeCoin(
         uint _orderId
     ) external  onlyBuyer(_orderId) {
         require(escrows[_orderId].status == EscrowStatus.Funded,"Refund not approved");
-             uint8 _decimals = 18; //Wei
-        uint256 _value = escrows[_orderId].value * 10 ** _decimals ;
+        uint256 _value = escrows[_orderId].value ;
         address _buyer = escrows[_orderId].buyer;
         delete escrows[_orderId];
         (bool sent, ) = payable(address(_buyer)).call{value: _value }("");
@@ -175,5 +167,4 @@ enum EscrowStatus {
 
         emit EscrowDisputeResolved(_orderId);
     }
-
 }
--- contracts/PeerPal_flattened_Verified.sol
+++ contracts/PeerPal_flattened_Verified.sol
@@ -87,8 +87,7 @@ interface IERC20 {
 pragma solidity ^0.8.20;
 
 contract PeerPal {
-    uint256 public counter;
-    
+    uint256 public counter;   
     address public owner;
     IERC20 public _token;    
     uint256 public feeSeller;
@@ -133,7 +132,7 @@ enum EscrowStatus {
         Arbitration
     }
 
-    struct Escrow {
+struct Escrow {
         address payable buyer; 
         address payable seller; 
         uint256 value; 
@@ -182,12 +181,11 @@ enum EscrowStatus {
     ) external payable virtual {
         uint256 _orderId = counter + 1;
         require(msg.sender != _seller, "seller cannot be the same as buyer");
-
         uint8 _decimals = 18;
         uint256 _amountFeeBuyer = ((_value * (feeBuyer * 10 ** _decimals)) /
             (100 * 10 ** _decimals)) / 1000;
         feeBuyer = _amountFeeBuyer;
-        require((_value * 10 ** _decimals) <= msg.value, "Incorrect amount");
+        require((_value ) <= msg.value, "Incorrect amount");
 
         string memory _idImage = "NO IMAGE" ;
 
@@ -206,7 +204,6 @@ enum EscrowStatus {
         emit EscrowDeposit(_orderId, escrows[_orderId]);
 
     }
-
     function releaseEscrowNativeCoin(
         uint _orderId
     ) external onlyBuyer(_orderId) {
@@ -233,10 +230,8 @@ enum EscrowStatus {
 
 
         escrows[_orderId].status = EscrowStatus.Completed;
-
-
         (bool sent, ) = escrows[_orderId].seller.call{
-            value: (escrows[_orderId].value * 10 ** _decimals) - _amountFeeSeller
+            value: (escrows[_orderId].value ) - _amountFeeSeller
         }("");
         
         require(sent, "Transfer failed.");
@@ -245,14 +240,11 @@ enum EscrowStatus {
 
 
     }
-
-
    function refundBuyerNativeCoin(
         uint _orderId
     ) external  onlyBuyer(_orderId) {
         require(escrows[_orderId].status == EscrowStatus.Funded,"Refund not approved");
-             uint8 _decimals = 18; //Wei
-        uint256 _value = escrows[_orderId].value * 10 ** _decimals ;
+        uint256 _value = escrows[_orderId].value ;
         address _buyer = escrows[_orderId].buyer;
         delete escrows[_orderId];
         (bool sent, ) = payable(address(_buyer)).call{value: _value }("");
@@ -260,5 +252,4 @@ enum EscrowStatus {
 
         emit EscrowDisputeResolved(_orderId);
     }
-
 }
